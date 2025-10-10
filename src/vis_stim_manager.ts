9bb8f7385ff3fa59da484d6a149f6b189edf0b2e
Yuhui Zhao <yhzhao343@gmail.com>
2025-10-10 13:40:55 -0400

    add upload images and storing them

--- src/vis_stim_manager.ts
+++ src/vis_stim_manager.ts
@@ -64,58 +64,241 @@ export function prepVisStimCtrlPanel(
 
   parentElement.appendChild(control_panel_div);
 
-  const form = createDiv("form", control_panel_div, ["flat-form"]);
-  control_panel_div.appendChild(form);
+  const timing_form = createDiv("timing_form", control_panel_div, [
+    "flat-form",
+  ]);
+  control_panel_div.appendChild(timing_form);
 
   // const baseline_form_group = createDiv("baseline-form", form, ["form-group"]);
 
-  const pre_baseline_input_id = "pre_baseline_time_s";
-  const pre_baseline_label = createLabel(
-    "pre_baseline_time_span",
-    form,
-    pre_baseline_input_id,
+  const start_baseline_input_id = "start_baseline_time_s";
+  const start_baseline_label = createLabel(
+    "start_baseline_time_span",
+    timing_form,
+    start_baseline_input_id,
     [],
-    "Pre Baseline(s):",
+    "Start Baseline(s):",
   );
-  pre_baseline_label.setAttribute("style", objToStyleStr(label_style));
+  start_baseline_label.setAttribute("style", objToStyleStr(label_style));
 
-  const pre_baseline_input = createNumInput(
-    pre_baseline_input_id,
-    form,
+  const start_baseline_input = createNumInput(
+    start_baseline_input_id,
+    timing_form,
     [],
     30,
     300,
-    10,
+    1,
     start_config.start_baseline_s,
   );
-  pre_baseline_input.setAttribute(
+  start_baseline_input.setAttribute(
     "style",
     objToStyleStr({ ...input_style, width: "3em" }),
   );
 
-  const post_baseline_input_id = "baseline_time_s";
-  const post_baseline_label = createLabel(
-    "post_baseline_time_span",
-    form,
-    post_baseline_input_id,
+  const end_baseline_input_id = "end_baseline_time_s";
+  const end_baseline_label = createLabel(
+    "end_baseline_time_span",
+    timing_form,
+    end_baseline_input_id,
     [],
-    "Post Baseline(s):",
+    "End Baseline(s):",
   );
-  post_baseline_label.setAttribute("style", objToStyleStr(label_style));
+  end_baseline_label.setAttribute("style", objToStyleStr(label_style));
 
-  const post_baseline_input = createNumInput(
-    post_baseline_input_id,
-    form,
+  const end_baseline_input = createNumInput(
+    end_baseline_input_id,
+    timing_form,
     [],
     30,
     300,
-    10,
+    1,
     start_config.start_baseline_s,
   );
-  post_baseline_input.setAttribute(
+  end_baseline_input.setAttribute(
     "style",
     objToStyleStr({ ...input_style, width: "3em" }),
   );
+
+  const start_fixation_input_id = "start_fixation_time_ms";
+  const start_fixation_label = createLabel(
+    "start_fixation_time_span",
+    timing_form,
+    start_fixation_input_id,
+    [],
+    "Start Fixation (ms):",
+  );
+  start_fixation_label.setAttribute("style", objToStyleStr(label_style));
+
+  const start_fixation_input = createNumInput(
+    start_fixation_input_id,
+    timing_form,
+    [],
+    1000,
+    10000,
+    1,
+    start_config.start_fixation_ms,
+  );
+  start_fixation_input.setAttribute(
+    "style",
+    objToStyleStr({ ...input_style, width: "3.2em" }),
+  );
+
+  const end_white_input_id = "end_white_time_ms";
+  const end_white_label = createLabel(
+    "end_white_time_span",
+    timing_form,
+    end_white_input_id,
+    [],
+    "End White (ms):",
+  );
+  end_white_label.setAttribute("style", objToStyleStr(label_style));
+
+  const end_white_input = createNumInput(
+    end_white_input_id,
+    timing_form,
+    [],
+    1000,
+    10000,
+    1,
+    start_config.end_white_ms,
+  );
+  end_white_input.setAttribute(
+    "style",
+    objToStyleStr({ ...input_style, width: "3.2em" }),
+  );
+
+  start_baseline_input.onchange = (event: Event) => {
+    if (event.target) {
+      if ("value" in event.target) {
+        if (typeof event.target.value === "number") {
+          start_config.start_baseline_s = event.target.value;
+        } else if (typeof event.target.value === "string") {
+          start_config.start_baseline_s = parseFloat(event.target.value);
+        }
+      }
+    }
+    start_baseline_input.value = start_config.start_baseline_s.toString();
+    updateSearchURL(start_config);
+  };
+
+  end_baseline_input.onchange = (event: Event) => {
+    if (event.target) {
+      if ("value" in event.target) {
+        if (typeof event.target.value === "number") {
+          start_config.end_baseline_s = event.target.value;
+        } else if (typeof event.target.value === "string") {
+          start_config.end_baseline_s = parseFloat(event.target.value);
+        }
+      }
+    }
+    end_baseline_input.value = start_config.end_baseline_s.toString();
+    updateSearchURL(start_config);
+  };
+
+  start_fixation_input.onchange = (event: Event) => {
+    if (event.target) {
+      if ("value" in event.target) {
+        if (typeof event.target.value === "number") {
+          start_config.start_fixation_ms = event.target.value;
+        } else if (typeof event.target.value === "string") {
+          start_config.start_fixation_ms = parseFloat(event.target.value);
+        }
+      }
+    }
+    start_fixation_input.value = start_config.start_fixation_ms.toString();
+    updateSearchURL(start_config);
+  };
+
+  end_white_input.onchange = (event: Event) => {
+    if (event.target) {
+      if ("value" in event.target) {
+        if (typeof event.target.value === "number") {
+          start_config.end_white_ms = event.target.value;
+        } else if (typeof event.target.value === "string") {
+          start_config.end_white_ms = parseFloat(event.target.value);
+        }
+      }
+    }
+    end_white_input.value = start_config.end_white_ms.toString();
+    updateSearchURL(start_config);
+  };
+
+  const button_style = {
+    display: "block",
+    height: "6vh",
+    "padding-left": "26px",
+    "padding-right": "26px",
+    "font-size": "2em",
+    cursor: "pointer",
+    "text-align": "center",
+    "margin-right": "10px",
+    "border-radius": "3vh",
+    border: "none",
+    color: "white",
+    "font-family": "Arial, sans-serif",
+    "background-color": "#5755d9",
+  };
+  const add_vis_stim_in = document.createElement("input");
+  add_vis_stim_in.setAttribute("type", "file");
+  add_vis_stim_in.setAttribute("id", "vis-stim-upload");
+  add_vis_stim_in.setAttribute("style", "display:none");
+  add_vis_stim_in.setAttribute("accept", "image/*");
+  add_vis_stim_in.multiple = true;
+  // add_vis_stim_in.setAttribute("name", "files[]");
+  add_vis_stim_in.onchange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    const files = target.files as FileList;
+    if (files.length > 0) {
+      addVisStimSet(files);
+    }
+  };
+
+  const add_vis_sim_btn = document.createElement("button");
+  add_vis_sim_btn.setAttribute("style", objToStyleStr(button_style));
+  add_vis_sim_btn.innerHTML = "+ Stim Set";
+  timing_form.appendChild(add_vis_sim_btn);
+  add_vis_sim_btn.onclick = () => {
+    add_vis_stim_in.click();
+  };
+
+  const vis_stim = createDiv("vis_stim", control_panel_div, ["flat-flex-col"]);
+  vis_stim.setAttribute("style", "width:100%");
+  control_panel_div.appendChild(vis_stim);
+
+  function addVisStimSet(files: FileList) {
+    const id_num = vis_stim.children.length;
+    const stim_row_div = createDiv(`Vis-stim-${id_num}`, vis_stim, [
+      "vis-stim-row",
+    ]);
+    for (let i = 0; i < files.length; i++) {
+      const img_card_id = `${id_num}-${i}`;
+      const card = createDiv(`img-card-${img_card_id}`, stim_row_div, ["card"]);
+      card.setAttribute("style", "width:10vw");
+      const header = createDiv(`img-card-header-${img_card_id}`, card, [
+        "card-header",
+      ]);
+      const header_content = createDiv(
+        `img-card-header-content-${img_card_id}`,
+        header,
+        ["card-title", "h6"],
+        files[i].name,
+      );
+      const card_img = createDiv(`img-card-img-${img_card_id}`, card, [
+        "card-image",
+      ]);
+      const reader = new FileReader();
+      reader.readAsDataURL(files[i]);
+      reader.onload = (e: any) => {
+        const img = document.createElement("img");
+        img.setAttribute("src", e.target?.result);
+        img.classList.add("fit");
+        card_img.appendChild(img);
+      };
+    }
+  }
+
+  // add_vis_stim_set.innerHTML = "+ Stim Set";
+  // vis_stim.appendChild(add_vis_stim_set);
 }
 
 function createLabel(
